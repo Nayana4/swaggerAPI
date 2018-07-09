81c188d886b7e40a2adc14b2b2f659f1a7e47376
NayanaC <nayana.chandrashekar@fireeye.com>
2018-07-09 17:34:40 +0000

    Generated code by SwaggerHub

---
+++ python-flask/fenet_server/test/test_default_controller.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from fenet_server.test import BaseTestCase
+
+
+class TestDefaultController(BaseTestCase):
+    """DefaultController integration test stubs"""
+
+    def test_fecontent_get_sc_image(self):
+        """Test case for fecontent_get_sc_image
+
+        Gets security content image
+        """
+        response = self.client.open(
+            '/fenet/MetaData/1.0.0/fecontent/{scParameters}'.format(scParameters='scParameters_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_femeta_get_meta_data(self):
+        """Test case for femeta_get_meta_data
+
+        Gets Meta Data
+        """
+        response = self.client.open(
+            '/fenet/MetaData/1.0.0/femeta/{metaParameters}'.format(metaParameters='metaParameters_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
